8cf52b676a4fd8fe53ad4a1b528c89a07e176c25
Trohat <petr.sed@centrum.cz>
2021-01-01 23:10:39 +0100

    Day 1

---
+++ D1/index.js
@@ -0,0 +1,48 @@
+console.log("funguju");
+
+const splitLines = (data) => data.split(String.fromCharCode(10));
+
+const prepare = data => data.map(d => +d);
+
+Array.prototype.sum = function () {
+    return this.reduce((a,b) => a+b, 0);
+}
+
+const task1 = data => data.sum();
+
+const task2 = data => {
+    const reached = new Set;
+    let sum = 0;
+    while (true) {
+        for (const n of data) {
+            sum += n;
+            if (reached.has(sum)) {
+                return sum;
+            }
+            reached.add(sum);
+        }
+    }
+}
+
+let testdata = `+1
+-2
++3
++1`;
+
+inputdata = prepare(splitLines(inputdata));
+
+console.log(inputdata);
+
+testdata = prepare(splitLines(testdata));
+
+console.log("");
+
+doEqualTest(task1(testdata), 3);
+
+console.log("Task 1: " + task1(inputdata));
+
+console.log("");
+
+doEqualTest(task2(testdata), 2);
+
+console.log("Task 2: " + task2(inputdata));
