f6cf391cba87904df9469b3b3158e2f4d0472c78
Trohat <petr.sed@centrum.cz>
2021-01-01 09:17:44 +0100

    Template, tests & README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2018
+
+Codes from [Christmas coding event](https://adventofcode.com/2018). 
+
+#### Useful functionalities: 
+
+...nothing yet...
---
+++ template/index.js
@@ -0,0 +1,33 @@
+console.log("funguju");
+
+const splitLines = (data) => data.split(String.fromCharCode(10));
+
+const prepare = data => data;
+
+const task1 = (data) => {
+    
+};
+
+const task2 = data => {
+    
+}
+
+let testdata = ``;
+
+inputdata = prepare(splitLines(inputdata));
+
+console.log(inputdata);
+
+testdata = prepare(splitLines(testdata));
+
+console.log("");
+
+//doEqualTest(task1(testdata), 7);
+
+//console.log("Task 1: " + task1(inputdata));
+
+console.log("");
+
+//doEqualTest(task2(testdata), 336);
+
+//console.log("Task 2: " + task2(inputdata));
