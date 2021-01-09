bbc10c3975a26b36a8016ec20b9ca8c3f97df389
Trohat <petr.sed@centrum.cz>
2021-01-09 22:16:56 +0100

    Day 17 first working version

--- D17/index.js
+++ D17/index.js
@@ -43,7 +43,7 @@ const prepare = data => {
         grid.push([]);
         let str = "";
         for (let j = minX; j <= maxX; j++) {
-            grid[i].push({ isWall: false, isWater: false });
+            grid[i].push({ isWall: false, isWater: false, cameFrom: null, beenDown: false, beenLeft: false, beenRight: false, twoWays: false, firstReturned: false });
             str += ".";
         }
         strGrid.push(str);
@@ -74,8 +74,26 @@ const task1 = ([grid, strGrid, toInspectArr]) => {
         return !(grid[y][x].isWall || grid[y][x].isWater);
     };
 
-    //TO DELETE
-    grid[6][6].isWall = false;
+    const goBack = (x,y) => {
+        let found = false;
+        while (!found) {
+            let searching = grid[y][x];
+            if (!searching.twoWays) {
+                if (searching.beenDown && searching.beenRight && searching.beenLeft) ({x,y} = searching.cameFrom);
+                else found = true;
+            } 
+            else // two ways field
+            if (searching.firstReturned) ({x,y} = searching.cameFrom);
+            else {
+                searching.firstReturned = true;
+                return;
+            }
+        }
+        if (found) toInspectArr.push({x, y, state: "going back"});
+        else console.warn("goBack working unexpectedly");
+    };
+
+    //grid[6][6].isWall = false; //TO DELETE
 
     while (toInspectArr.length > 0) {
         let toInspect = toInspectArr.shift();
@@ -84,49 +102,64 @@ const task1 = ([grid, strGrid, toInspectArr]) => {
         let state = toInspect.state;
 
         // reached the end of sand
-        if (y === grid.length) continue;
+        if (y === grid.length - 1) continue;
 
         // can go down
+        grid[y][x].beenDown = true;
         if (isFree(y + 1, x)) {
             grid[y + 1][x].isWater = true;
+            grid[y + 1][x].cameFrom = { x, y };
             strGrid[y + 1] = strGrid[y + 1].setCharAt(x, "|");
             toInspectArr.push({ y: y + 1, x, state: "freefall" });
             continue;
         }
 
-        // felt on water
-        if (grid[y+1][x].isWater) continue;
+        // fell on water
+        //if (state === "freefall" && grid[y+1][x].isWater) continue;
 
-        // felt on clay
-        if (state === "freefall") {
+        // felt on clay 
+        grid[y][x].beenRight = true;
+        grid[y][x].beenLeft = true;
+        if (state === "freefall" || state === "going back") {
+            let possibleToMove = 0;
             if (isFree(y, x+1)) {
+                possibleToMove++;
                 grid[y][x + 1].isWater = true;
+                grid[y][x + 1].cameFrom = { x, y };
                 strGrid[y] = strGrid[y].setCharAt(x + 1, "~");
                 toInspectArr.push({ y, x: x + 1, state: "going right" });
             }
             if (isFree(y, x-1)) {
+                possibleToMove++;
                 grid[y][x - 1].isWater = true;
+                grid[y][x - 1].cameFrom = { x, y };
                 strGrid[y] = strGrid[y].setCharAt(x - 1, "~");
                 toInspectArr.push({ y, x: x - 1, state: "going left" });
             }
+            if (possibleToMove === 0) goBack(x,y);
+            if (possibleToMove === 2) grid[y][x].twoWays = true;
             continue;
         }
-
+        
         if (state === "going right") {
             if (isFree(y, x + 1)) {
                 grid[y][x + 1].isWater = true;
+                grid[y][x + 1].cameFrom = { x, y };
                 strGrid[y] = strGrid[y].setCharAt(x + 1, "~");
                 toInspectArr.push({ y, x: x + 1, state: "going right" });
             }
+            else goBack(x,y);
             continue;
         }
-
+        
         if (state === "going left") {
             if (isFree(y, x - 1)) {
                 grid[y][x - 1].isWater = true;
+                grid[y][x - 1].cameFrom = { x, y };
                 strGrid[y] = strGrid[y].setCharAt(x - 1, "~");
                 toInspectArr.push({ y, x: x - 1, state: "going left" });
             }
+            else goBack(x,y);
             continue;
         }
 
@@ -148,7 +181,7 @@ x=498, y=10..13
 x=504, y=10..13
 y=13, x=498..504`;
 
-//inputdata = prepare(splitLines(inputdata));
+inputdata = prepare(splitLines(inputdata));
 
 //console.log(inputdata);
 
@@ -158,9 +191,9 @@ testdata = prepare(splitLines(testdata));
 
 console.log("");
 
-doEqualTest(task1(testdata), undefined);
+//doEqualTest(task1(testdata), undefined);
 
-//console.log("Task 1: " + task1(inputdata));
+console.log("Task 1: " + task1(inputdata));
 
 console.log("");
 
