dcb35f567e453c315e7b809e8d66522a0d239dee
Trohat <petr.sed@centrum.cz>
2021-01-13 13:50:08 +0100

    Day 24 Task 1

--- D24/index.js
+++ D24/index.js
@@ -1,99 +1,165 @@
 console.log("AOC 2018 - Day 24: Immune System Simulator 20XX");
 
-const splitLines = (data) => data.split(String.fromCharCode(10) + String.fromCharCode(10));
+const splitLines = (data) => data.split(String.fromCharCode(10));
+
+const IMMUNE_SYSTEM = "immuneSystem";
+const INFECTION = "infection";
+const IMMUNE = "immune";
+const WEAK = "weak";
+const DEBUG = "debug";
 
 const prepare = data => {
-    const readArr = arr => {
-        let re = /(\d+) units each with (\d+) hit points (\([a-z ,;]+\) |)with an attack that does (\d+) (\w+) damage at initiative (\d+)/
-        let traitsRE = /(weak|immune) to ([\w, ]+)$/;
-        arr = arr.split(String.fromCharCode(10));
-        arr.shift();
-        let output = [];
-        for (const line of arr) {
-            let [ , units, hp, traits, attack, attackType, initiative ] = re.exec(line);
-            if (traits.length > 0) {
-                traits = traits.slice(1, traits.length - 2);
-                traits = traits.split(";");
-                let traitsObj = {};
-                for (const line of traits) {
-                    let [ , what, names ] = traitsRE.exec(line);
-                    traitsObj[what] = names;
-                }
-                traits = traitsObj;
-            } else traits = null;
-            let effectivePower = units * attack;
-            output.push({ effectivePower, units: +units, hp: +hp, attack: +attack, attackType, initiative: +initiative, traits, target: null });
-        }
-        return output;
-    }
+    let re = /(\d+) units each with (\d+) hit points (\([a-z ,;]+\) |)with an attack that does (\d+) (\w+) damage at initiative (\d+)/
+    let traitsRE = /(weak|immune) to ([\w, ]+)$/;
 
-    const immuneSystem = readArr(data[0]);
-    const infection = readArr(data[1]);
-    
-    return  [ immuneSystem, infection ];
+    const system = [];
+    let type;
+    let id;
+    for (const line of data) {
+        if (line === "") continue;
+        if (line.trim() === "Immune System:") {
+            type = IMMUNE_SYSTEM;
+            id = 1;
+            continue;
+        }
+        if (line.trim() === "Infection:") {
+            type = INFECTION;
+            id = 1;
+            continue;
+        }
+
+        let [, units, hp, traits, attack, attackType, initiative] = re.exec(line);
+        if (traits.length > 0) {
+            traits = traits.slice(1, traits.length - 2);
+            traits = traits.split(";");
+            let traitsObj = {};
+            for (const line of traits) {
+                let [, what, names] = traitsRE.exec(line);
+                traitsObj[what] = names;
+            }
+            traits = traitsObj;
+        } else traits = null;
+        let effectivePower = units * attack;
+        system.push({ type, id, initiative: +initiative, effectivePower, units: +units, hp: +hp, attack: +attack, attackType, initiative: +initiative, traits, target: null, chosen: false });
+        id++;
+    }
+    return system;
 };
 
-const task1 = ([ immuneSystem, infection ]) => { 
-    const sortingFunction = (a,b) => {
-        if (b.units * b.attack === a.units* a.attack) return b.initiative - a.initiative;
-        return b.units * b.attack - a.units* a.attack;
+const task1 = (system, debug) => {
+    const effectivePowerSortingFunction = (a,b) => {
+        if (a.effectivePower === b.effectivePower) return b.initiative - a.initiative;
+        return b.effectivePower - a.effectivePower;
+    };
+
+    const initiativeSortingFunction = (a, b) => b.initiative - a.initiative;
+
+    const logSortingFunction = (a,b) => {
+        if (a.type === b.type) return a.id - b.id;
+        if (a.type === IMMUNE_SYSTEM) return -1;
+        return 1;
+    };
+
+    const log = (text) => {
+        if (debug === DEBUG) console.log(text);
     };
 
     const damageCoefficient = (attackType, traits) => {
         if (traits === null) return 1;
-        if ("immune" in traits && traits.immune.indexOf(attackType) !== -1) return 0;
-        if ("weak" in traits && traits.weak.indexOf(attackType) !== -1) return 2;
+        if (IMMUNE in traits && traits.immune.indexOf(attackType) !== -1) return 0;
+        if (WEAK in traits && traits.weak.indexOf(attackType) !== -1) return 2;
         return 1;
     }
 
-    const selectTarget = (unit, enemyGroup) => {
+    const selectTarget = (unit, system) => {
         let maxDamage = 0;
-        let enemyIndex = -1;
-        enemyGroup.forEach((enemyUnit, index) => {
+        let target = null;
+        let enemyEffectivePower = 0;
+        let enemyInitative = 0;
+        system.forEach((enemyUnit, index) => {
+            if (unit.type === enemyUnit.type || enemyUnit.chosen) return;
             let actualDamage = unit.effectivePower * damageCoefficient(unit.attackType, enemyUnit.traits);
-            if (actualDamage > maxDamage) {
+            if (actualDamage > 0 && actualDamage === maxDamage && enemyEffectivePower === enemyUnit.effectivePower && enemyUnit.initiative > enemyInitative
+                || actualDamage > 0 && actualDamage === maxDamage && enemyUnit.effectivePower > enemyEffectivePower
+                || actualDamage > maxDamage) {
+                target = enemyUnit;
                 maxDamage = actualDamage;
-                enemyIndex = index;
+                enemyEffectivePower = enemyUnit.effectivePower;
+                enemyInitative = enemyUnit.initiative;
+                let type = unit.type === IMMUNE_SYSTEM ? "Immune System" : "Infection";
+                log(type + " group " + unit.id + " would deal defending group " + enemyUnit.id + " " + actualDamage + " damage.");
             }
         });
         if (maxDamage === 0) return null;
-        return enemyIndex;
+        target.chosen = true;
+        return target;
     };
 
     const attack = (unit, enemy) => {
         if (unit.units < 0) return;
+        unit.effectivePower = unit.units * unit.attack;
         let damage = unit.effectivePower * damageCoefficient(unit.attackType, enemy.traits);
         let killed = Math.floor(damage / enemy.hp);
+        let type = unit.type === IMMUNE_SYSTEM ? "Immune System" : "Infection";
+        log(type + " group " + unit.id + " attacks defending group " + enemy.id + ", killing " + killed + " units.");
         enemy.units -= killed;
     };
 
-   
     end = false;
+    let round = 0;
     while (!end) {
-        immuneSystem.sort(sortingFunction);
-        infection.sort(sortingFunction);
-        for (const unit of immuneSystem) {
-            unit.target = selectTarget(unit, infection);
-        }
-        for (const unit of infection) {
-            unit.target = selectTarget(unit, immuneSystem);
-        }
-        for (const unit of immuneSystem) {
-            attack(unit, infection[unit.target]);
-        }
-        for (const unit of infection) {
-            attack(unit, immuneSystem[unit.target]);
+        round++;
+        log("");
+        log("Round " + round + ":")
+        
+        system.sort(effectivePowerSortingFunction);
+        
+        for (const unit of system) {
+            unit.target = selectTarget(unit, system);
         }
 
-        console.log(immuneSystem);
-        console.log(infection);
-        end = true;
+        log("");
+        system.sort(initiativeSortingFunction);
+
+        for (const unit of system) {
+            if (unit.target) attack(unit, unit.target);
+        }
+
+        system = system.filter( unit => unit.units > 0);
+
+        let immunity = false;
+        let infection = false;
+        let firstInfection = true;
+        system.sort(logSortingFunction);
+        log("Immune System:");
+
+        // reset effective power + which units were chosen and log the state
+        for (const unit of system) {
+            unit.effectivePower = unit.units * unit.attack;
+            unit.chosen = false;
+            if (unit.type === IMMUNE_SYSTEM) immunity = true;
+            if (unit.type === INFECTION) {
+                if (firstInfection) {
+                    firstInfection = false;
+                    infection = true;
+                    if (!immunity) log("No groups remain.");
+                    log("Infection:");
+                }
+            };
+            log("Group " + unit.id + " contains " + unit.units + ".");
+        }
+        if (!infection) log("No groups remain.");
+        if (!(infection && immunity)) end = true;
+
     }
-    */
+    let totalUnits = 0;
+    for (const unit of system) totalUnits += unit.units;
+
+    return totalUnits;
 };
 
 const task2 = data => {
-    
+
 }
 
 let testdata = `Immune System:
@@ -106,13 +172,11 @@ Infection:
 
 inputdata = prepare(splitLines(inputdata));
 
-console.log(inputdata);
-
 testdata = prepare(splitLines(testdata));
 
 console.log("");
 
-//doEqualTest(task1(testdata), 7);
+doEqualTest(task1(testdata, DEBUG), 5216);
 
 console.log("Task 1: " + task1(inputdata));
 
